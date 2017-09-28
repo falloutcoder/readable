53af1e61c1ae0a49d0c0c84f19a54b6666be0562
falloutcoder <hamezgul@gmail.com>
2017-09-28 13:06:29 +0500

    Update backend code

--- backend/posts.js
+++ backend/posts.js
@@ -57,7 +57,7 @@ function getAll (token) {
   return new Promise((res) => {
     const posts = getData(token)
     let keys = Object.keys(posts)
-    let filtered_keys = keys.filter(key => !posts.deleted)
+    let filtered_keys = keys.filter(key => !posts[key].deleted)
     res(filtered_keys.map(key => posts[key]))
   })
 }
@@ -65,6 +65,7 @@ function getAll (token) {
 function add (token, post) {
   return new Promise((res) => {
     let posts = getData(token)
+
     posts[post.id] = {
       id: post.id,
       timestamp: post.timestamp,
@@ -85,35 +86,35 @@ function vote (token, id, option) {
     let posts = getData(token)
     post = posts[id]
     switch(option) {
-        case "upVote":
-            post.voteScore = post.voteScore + 1
-            break
-        case "downVote":
-            post.voteScore = post.voteScore - 1
-            break
-        default:
-            console.log(`posts.vote received incorrect parameter: ${option}`)
+      case "upVote":
+        post.voteScore = post.voteScore + 1
+        break
+      case "downVote":
+        post.voteScore = post.voteScore - 1
+        break
+      default:
+        console.log(`posts.vote received incorrect parameter: ${option}`)
     }
     res(post)
   })
 }
 
 function disable (token, id) {
-    return new Promise((res) => {
-      let posts = getData(token)
-      posts[id].deleted = true
-      res(posts[id])
-    })
+  return new Promise((res) => {
+    let posts = getData(token)
+    posts[id].deleted = true
+    res(posts[id])
+  })
 }
 
 function edit (token, id, post) {
-    return new Promise((res) => {
-        let posts = getData(token)
-        for (prop in post) {
-            posts[id][prop] = post[prop]
-        }
-        res(posts[id])
-    })
+  return new Promise((res) => {
+    let posts = getData(token)
+    for (prop in post) {
+      posts[id][prop] = post[prop]
+    }
+    res(posts[id])
+  })
 }
 
 module.exports = {
